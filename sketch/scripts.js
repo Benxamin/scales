cfb5cbed73db17a6c3dd5799877a3511650d42a5
Benjamin <benxaminabc@protonmail.com>
2023-02-26 20:42:09 -0700

    12-bb Adds enable sound button.

--- sketch/scripts.js
+++ sketch/scripts.js
@@ -359,6 +359,6 @@ const handleRootNoteChange = function(changeEvent) {
 };
 
 // Wire up functionality.
-// soundEnableButtonElement.addEventListener("click", handleEnableSoundClick);
+soundEnableButtonElement.addEventListener("click", handleEnableSoundClick);
 scaleSelectorElement.addEventListener("change", handleScaleTypeChange);
 rootNoteSelectorElement.addEventListener("change", handleRootNoteChange);
